28e029ac5723d061e3fbd0fc834a5f2491420c30
azoraw <al.gmail@gmail.com>
2023-11-11 14:01:42 +0100

    Add from-to case

--- src/main/java/azoraw/Main.java
+++ src/main/java/azoraw/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         final CronExpressionParser cronExpressionParser = new CronExpressionParser();
         //final String parsedExpression = cronExpressionParser.parse(args[0]);
-        final String parsedExpression = cronExpressionParser.parse("* * * * * /");
+        final String parsedExpression = cronExpressionParser.parse("2-4 * * * * /");
         System.out.println(parsedExpression);
     }
 }
--- src/main/java/azoraw/NumericParser.java
+++ src/main/java/azoraw/NumericParser.java
@@ -1,23 +1,41 @@
 package azoraw;
 
+import static java.lang.Integer.parseInt;
+
 class NumericParser {
 
     String parseMinute(String input) {
-        return parseStar(CronRule.MINUTE);
+        //return parseStar(CronRule.MINUTE);
+        return parseFromTo(input);
     }
+
     String parseHour(String input) {
         return parseStar(CronRule.HOUR);
     }
+
     String parsDayOfMonth(String input) {
         return parseStar(CronRule.DAY_OF_MONTH);
     }
+
     String parseMonth(String input) {
         return parseStar(CronRule.MONTH);
     }
+
     String parseDayOfWeek(String input) {
         return parseStar(CronRule.DAY_OF_WEEK);
     }
 
+    private static String parseFromTo(String input) {
+        final String[] split = input.split("-");
+        int from = parseInt(split[0]);
+        int to = parseInt(split[1]);
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (int i = from; i <= to; i++) {
+            stringBuilder.append(i);
+            stringBuilder.append(" ");
+        }
+        return stringBuilder.toString();
+    }
 
     private static String parseStar(CronRule cronRule) {
         final StringBuilder stringBuilder = new StringBuilder();
